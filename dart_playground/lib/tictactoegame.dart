3b577ed2d974409eeb24bfcac93f0c380bdcc069
steineca <steineca@rose-hulman.edu>
2022-12-13 13:49:27 -0500

    finished tictactoe!

--- dart_playground/lib/tictactoegame.dart
+++ dart_playground/lib/tictactoegame.dart
@@ -1,5 +1,6 @@
 import 'dart:ffi';
 import 'dart:io';
+import 'package:test/test.dart';
 
 enum Mark { x, o, none }
 
@@ -30,7 +31,22 @@ class TicTacToeGame {
     checkBoard();
   }
 
-  void checkBoard() {}
+  void checkBoard() {
+    String winconsstring =
+        "${board[0]}${board[1]}${board[2]}|${board[3]}${board[4]}${board[5]}|${board[6]}${board[7]}${board[8]}|${board[0]}${board[3]}${board[6]}|${board[1]}${board[4]}${board[7]}|${board[2]}${board[5]}${board[8]}|${board[0]}${board[4]}${board[8]}|${board[2]}${board[4]}${board[6]}";
+
+    if (winconsstring.contains("Mark.xMark.xMark.x")) {
+      state = GameState.xWon;
+      return;
+    } else if (winconsstring.contains("Mark.oMark.oMark.o")) {
+      state = GameState.oWon;
+      return;
+    } else if (!board.contains(Mark.none)) {
+      state = GameState.tie;
+      return;
+    }
+    return;
+  }
 
   String boardAsString() {
     String boardString = "";
@@ -50,13 +66,29 @@ class TicTacToeGame {
     }
     return boardString;
   }
+
+  String stateString() {
+    String statestring = "";
+    if (state == GameState.xTurn) {
+      statestring = "X's Turn";
+    } else if (state == GameState.oTurn) {
+      statestring = "O's Turn";
+    } else if (state == GameState.xWon) {
+      statestring = "X Wins!";
+    } else if (state == GameState.oWon) {
+      statestring = "O Wins!";
+    } else if (state == GameState.tie) {
+      statestring = "It's A Tie!";
+    }
+    return statestring;
+  }
 }
 
 void main() {
   TicTacToeGame game = new TicTacToeGame();
   print("Testing TicTacToeGame");
   while (true) {
-    print(game.state);
+    print("\n${game.stateString()}");
     print(game.boardAsString());
     if (game.state == GameState.oTurn || game.state == GameState.xTurn) {
       print("Which square do you wish to play?");
@@ -67,6 +99,9 @@ void main() {
           game.selectSquare(picked);
         }
       } catch (e) {}
+    } else {
+      print("Good Game!");
+      break;
     }
   }
 }
